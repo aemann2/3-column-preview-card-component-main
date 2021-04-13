c791bf27b795cd87f20417faf008568c3072e99c
Adam Mann <aemann2@gmail.com>
2021-04-13 13:56:16 -0700

    added  custom vtailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        primary: {
+          orange: 'hsl(31, 77%, 52%)',
+          cyan: 'hsl(184, 100%, 22%)',
+          darkCyan: 'hsl(179, 100%, 13%)',
+        },
+        neutral: {
+          white: 'hsla(0, 0%, 100%, 0.75)',
+          lightGray: 'hsl(0, 0%, 95%)',
+        },
+      },
       fontFamily: {
-        body: ['Nunito'],
+        body: ['"Big Shoulders Display"'],
+        heading: ['"Lexend Deca"'],
       },
     },
   },
